382b1141256421da5e65ed7d2725f65eebb68fc7
Powmer <punchabunch3000@gmail.com>
2025-12-05 10:10:46 -0300

    Add temperature adjustment logic in enviar.py
    Implement temperature adjustment functions and logging.

---
+++ Pasta/enviar.py
@@ -0,0 +1,21 @@
+barco = []
+md=-15
+nm=-20
+def alterar_temp(md,nm):
+    dif = -1*(md - nm)
+    print(dif)
+    if dif<0:
+        for i in range(-1*(dif)):
+            app_menos()
+    else:
+        for i in range(dif):
+            app_mais()
+
+
+def app_mais():
+    barco.append("mais")
+def app_menos():
+    barco.append("menos")
+alterar_temp(md,nm)
+print(barco)
+
